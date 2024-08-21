b50c4f8395fbaad9a826a746e2be77eec80d7d7d
Hardik Tanna <hardik.tanna@somaiya.edu>
2024-08-21 13:36:12 -0400

    Add navigation

--- src/components/pages/AddCreator.tsx
+++ src/components/pages/AddCreator.tsx
@@ -1,8 +1,11 @@
+import { useNavigate } from "react-router-dom";
 
 export const AddCreator = () => {
+    const navigate = useNavigate();
   return (
     <form onSubmit={(event)=>{
         event.preventDefault();
+        navigate("/");
     }}>
         <button className="btn btn-primary" type="submit">Submit</button>
 
